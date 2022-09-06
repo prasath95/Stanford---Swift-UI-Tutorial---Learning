4f7a2e24abeab4e7fce3d20982af4806cde95a99
prasath95 <6sivaprasath@gmail.com>
2022-09-06 20:37:53 +0530

    Initial Commit

---
+++ IndieFirstApp/IndieFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndieFirstAppApp.swift
+//  IndieFirstApp
+//
+//  Created by Prasath Sivanathan on 2022-09-06.
+//
+
+import SwiftUI
+
+@main
+struct IndieFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
